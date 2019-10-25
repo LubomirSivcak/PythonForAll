e602278b753c29db940ae4162c853d9ae4e4c72a
LubomirSivcak <lubomir.sivcak@tul.cz>
2019-10-25 11:14:55 +0200

    test

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
-test = np.range(1000)
-plt.plot(test)
-plt.show()
+
+print("arange", np.arange(1.1,10,2.1))
+print("linspace", np.linspace(1.1,10,4))
+print("",np.array([[1,2], [4,5]]))
+
+import pandas as pd
+
+dates = pd.date_range('20130101', periods=6)
+df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
+print(df.head())
